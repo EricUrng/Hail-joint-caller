01460c7e548f95c2414dbafad82347c4c1dafc1f
Eric Urng <e.urng@garvan.org.au>
2022-01-28 12:25:48 +1100

    including get_sample_name_map function

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ import os
 import sys
 from cli import make_argparser
 from run_combiner import run_combiner
+from get_sample_name_map import gen_sample_name_map
 
 def main(): 
 	parser = make_argparser()
-#	args = parse_args(parser)	
 
 	if parser.command == "joint_call":
 		print(run_combiner())
 	elif parser.command == "get_sample_name_map":
-		pass
+		gen_sample_name_map(parser)
 	elif parser.command == "complete_QC":
 		pass
 	else:
