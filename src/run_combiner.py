38e9d22f0e8cafbf13b1fd6607222d92fc1617c2
Eric Urng <e.urng@garvan.org.au>
2022-01-31 17:20:09 +1100

    fixed formatting of script

--- src/run_combiner.py
+++ src/run_combiner.py
@@ -6,6 +6,7 @@ def run_combiner(parser):
 	print("Running combiner...")
 
 	sample_name_map = os.path.abspath(parser.sample_name_map)
+	hail_home = '/home/eriurn/submit/python37_intel/lib/python3.7/site-packages/hail/' 
 	output_dir = os.path.abspath(parser.output_dir)
 
 	temp_bucket = output_dir
@@ -18,16 +19,16 @@ def run_combiner(parser):
 
 		f.write("import hail as hl\n")
 		f.write("hl.init()\n")
-		f.write(f"temp_bucket=file://{temp_bucket}\n")
-		f.write(f"output_file=file://{output_dir}\n")
-		f.write(f"path_to_input_list=file://{sample_name_map}\n")
+		f.write(f"temp_bucket='file://{temp_bucket}'\n")
+		f.write(f"output_file='file://{output_dir}'\n")
+		f.write(f"path_to_input_list='file://{sample_name_map}'\n")
 		f.write("inputs = []\n")
-		f.write("with hl.hadoop_open(path_to_input_list), 'r' as f:\n")
+		f.write("with hl.hadoop_open(path_to_input_list, 'r') as f:\n")
 		f.write("    for line in f:\n") 
 		f.write("        inputs.append(line.strip())\n")
 
-		f.write("hl.experimental.run_combiner(inputs, output_file=output_file, tmp_path=temp_bucket, reference_genome='GRCh38', use_genome_default_intervals=True, overwrite=True\n")
+		f.write(f"hl.experimental.run_combiner(inputs, output_file={output_file}, tmp_path={temp_bucket}, reference_genome='GRCh38', use_genome_default_intervals=True, overwrite=True)\n")
 
-	subprocess.run("spark-submit --archives pyspark_venv.tar.gz#python37_intel --jars $HAIL_HOME/backend/hail-all-spark.jar spark_submit_script.py",shell=True)
+	subprocess.run(f"spark-submit --archives ~/submit/pyspark_venv.tar.gz#python37_intel --jars {hail_home}/backend/hail-all-spark.jar {spark_submit_script}",shell=True)
 	print("Completed running combiner...")
 
