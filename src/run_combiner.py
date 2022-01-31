592568b0925562bd2f0bae981767a5b9e49352de
Eric Urng <ericurng@gmail.com>
2022-01-31 16:31:04 +1100

    fixed variable parsing and script writing

--- src/run_combiner.py
+++ src/run_combiner.py
@@ -2,32 +2,29 @@ import subprocess
 import os
 
 def run_combiner(parser):
+
 	print("Running combiner...")
 
 	with open("spark_submit_script.py", "w") as spark_submit_script:
-		spark_submit_script.write("import hail as hl")
-		spark_submit_script.write("hl.init()")
-		spark_submit_script.write(f"output_file={parser.output_dir}\n")
-		if parser.tmp_dir:
-			spark_submit_script.write(f"temp_bucket={parser.tmp_dir}\n")
-		else:
-			spark_submit_script.write(f"temp_bucket={parser.output_dir}\n")
 
-		spark_submit_script.write(f"path_to_input_list={parser.sample_name_map}\n")
+		sample_name_map = os.path.abspath(parser.sample_name_map)
+		output_dir = os.path.abspath(parser.output_dir)
+
+		temp_bucket = output_dir
+		if parser.tmp_dir:
+			temp_bucket = os.path.abspath(parser.tmp_dir)
+
+		spark_submit_script.write("import hail as hl\n")
+		spark_submit_script.write("hl.init()\n")
+		spark_submit_script.write(f"temp_bucket={temp_bucket})\n")
+		spark_submit_script.write(f"output_file={output_dir}\n")
+		spark_submit_script.write(f"path_to_input_list={sample_name_map}\n")
 		spark_submit_script.write("inputs = []\n")
 		spark_submit_script.write("with hl.hadoop_open(path_to_input_list), 'r' as f:\n")
 		spark_submit_script.write("    for line in f:\n") 
 		spark_submit_script.write("        inputs.append(line.strip())\n")
 
-		spark_submit_script.write("""hl.experimental.run_combiner(inputs,
-																  output_file=output_file,
-																  tmp_path=temp_bucket,
-																  reference_genome='GRCh38',
-																  use_genome_default_intervals=True,
-																  overwrite=True""")
+		spark_submit_script.write("""hl.experimental.run_combiner(inputs, output_file=output_file, tmp_path=temp_bucket, reference_genome='GRCh38', use_genome_default_intervals=True, overwrite=True""")
 
 	print("Completed running combiner...")
 
-	# Write to a new file called spark_submit_run_combiner.py
-	# and then write the same lines from the jupyter notebook to this file
-	# taking in the users command line arguments
