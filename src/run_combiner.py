1c1d232441563453149515f025366af9784d8895
Eric Urng <e.urng@garvan.org.au>
2022-01-31 15:43:01 +1100

    updating combiner

--- src/run_combiner.py
+++ src/run_combiner.py
@@ -6,3 +6,7 @@ def run_combiner():
 	subprocess.run("spark-submit --archives ~/pyspark_venv.tar.gz#python37_intel --master yarn --deploy-mode cluster test_script.py",shell=True,capture_output=True)
 	print("Completed running combiner...")
 	pass
+
+	# Write to a new file called spark_submit_run_combiner.py
+	# and then write the same lines from the jupyter notebook to this file
+	# taking in the users command line arguments
