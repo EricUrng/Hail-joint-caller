c050fcd94614d0807224fcaa2e7d903beb189a2b
Eric Urng <e.urng@garvan.org.au>
2022-02-11 13:50:55 +1100

    options, logger and comments

--- scripts/mt_to_vcf.py
+++ scripts/mt_to_vcf.py
@@ -1,14 +1,18 @@
-import hail as hl
+#---------------------------------------------------#
+#              Convert a MT into a VCF              #
+#---------------------------------------------------#
+
 import os
 import argparse
 import logging
+import hail as hl
 
 from gnomad.utils.vcf import adjust_vcf_incompatible_types
 from gnomad.utils.sparse_mt import default_compute_info
 
 def make_argparser():
     parser = argparse.ArgumentParser(
-        description = "A tool for converting a Hail mt to a VCF file"
+        description = "A tool for converting a Hail mt to a VCF"
     )
 
     parser.add_argument(
@@ -40,52 +44,94 @@ def make_argparser():
         help="name of the application"
     )
 
+	parser.add_argument(
+		"--hdfs",
+		action="store_true",
+		help="sample files are stored on the hdfs"
+	)
+
     return parser.parse_args()
 
 def main():
 
     parser = make_argparser()
-    path_to_mt = "file://" + os.path.abspath(parser.path_to_mt)
-    path_to_output = "file://" + os.path.abspath(parser.output)
 
-    print(path_to_mt)
-    print(path_to_output)
-    print(os.path.dirname(path_to_output))
+    # Initialise logger
+    init_log(os.path.abspath(parser.output))
 
+    # Prepend location according for files. Default is local.
+	if hasattr(parser, "on_hdfs"):
+		prepend_location = "hdfs://"
+	else:
+		prepend_location = "file://"
+
+    # Path to input mt
+    path_to_mt = prepend_location + os.path.abspath(parser.path_to_mt)
+
+    # Path to output VCF
+    path_to_output = prepend_location + os.path.abspath(parser.output)
+
+	# Set name for the Hail application
     if parser.app_name:
         name = parser.app_name
     else:
-        name = "Hail"
+        name = "mt_to_vcf"
 
+    logging.info("Initialising Hail")
     hl.init(
         app_name=name,
         log=os.getcwd()
     )
 
+	logging.info(f"Reading mt from {path_to_mt}")
 
+    # Read in mt to be converted
     mt = hl.read_matrix_table(path_to_mt)
 
+    logging.info('Converting mt to sites-only mt')
+
+    # Reference blocks of the sparse matrix has to be expanded 
     mt = hl.experimental.densify(mt)
 
+    # Filter for rows with at least one non-reference allele
     mt = mt.filter_rows((hl.len(mt.alleles) > 1) & (hl.agg.any(mt.LGT.is_non_ref())))
 
+    # Annotate site level DP
     mt = mt.annotate_rows(site_dp=hl.agg.sum(mt.DP))
 
+    # Annotate AN tag as ANS
     mt = mt.annotate_rows(ANS=hl.agg.count_where(hl.is_defined(mt.LGT)) * 2)
 
+    # Generate ht with GATK allele-specific info fields
     info_ht = default_compute_info(mt, site_annotations=True, n_partitions = parser.n_partitions)
-
     info_ht = info_ht.annotate(info=info_ht.info.annotate(DP=mt.rows()[info_ht.key].site_dp))
 
+    # Convert annotation into pipe-delimited strings
+    # All int64 are coerced into int32
     ht = adjust_vcf_incompatible_types(
         info_ht, 
         pipe_delimited_annotations=[]
     )
 
+    logging.info(f"Exporting sites-only VCF to {path_to_output}")
+
+    # Exporting ht to VCF
     hl.export_vcf(ht, path_to_output)
 
+    logging.info(f"Successfully exported")
 
 
+def init_log(path_to_output):
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y_%I_%M_%S_%p")
+    log_location = os.path.join(os.path.dirname(path_to_output), f"mt_to_vcf_{dt_string}.log")
+
+    logging.basicConfig(
+        filename=log_location, 
+        format="%(asctime)s %(message)s", 
+        datefmt="%d/%m/%Y %I:%M:%S %p - ",
+        level=logging.INFO
+    )
 
 if __name__ == "__main__":
     main()
